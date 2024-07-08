cd19255f8da0014a729e844f8e13e8f58af1ccc2
ahmed-sherbini <156031710+ahmed-sherbini@users.noreply.github.com>
2024-07-08 23:17:29 +0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -3,6 +3,11 @@ CREATE TABLE EUUM_UM_PREPROD.EXEC_TIME (
 	END_TIME TIMESTAMP NULL,
 	NAME VARCHAR2(100) NULL
 );
+   	v_start_time timestamp;
+   	v_end_time timestamp;
+
+	v_start_time := systimestamp;
+	v_end_time := systimestamp;
 
 	   INSERT INTO EXEC_TIME et (start_time, end_time, name) VALUES (v_start_time, v_end_time, 'TRG_AI_USER_ROLE_GROUP_TO_BO_ROLE');
 	   v_end_time := systimestamp;
